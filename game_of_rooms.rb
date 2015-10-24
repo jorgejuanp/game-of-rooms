2f1400a7dc35c6f7bdd8594a3deb907ca6e466ed
Jorge Juan <hi.jorgejuanp@gmail.com>
2015-10-24 14:36:13 +0200

    First commit

---
+++ game_of_rooms.rb
@@ -0,0 +1,6 @@
+#########################################
+# author: jorge juan perales
+# date: october 2015
+# exercise: Game of rooms
+# http://www.ironhack.com:3000/#/learning_unit/147
+#########################################
